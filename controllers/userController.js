4c9ed56a72737572eb7f95ab2199f577d1171821
DeepChandMishra <deepchandmishra90761@gmail.com>
2024-11-28 16:45:59 +0530

    user authentiation done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
 const userModel=require('../models/user');
 const userService=require('../services/userService');
 const {validationResult}=require('express-validator')
-
+const blacklistToken=require('../models/blaclistToken');
+const blaclistToken = require('../models/blaclistToken');
 module.exports.registerUser= async (req,res,next)=>{
     const error=validationResult(req);
     if(!error.isEmpty()){
@@ -36,6 +37,19 @@ module.exports.loginUser= async(req,res,next)=>{
     }
 
     const token=user.generateAuthToken();
-    
+    res.cookie('token',token);
     res.status(200).json({token,user});
+}
+
+module.exports.getUserProfile = async(req,res,next)=>{
+    res.status(200).json(req.user);
+}
+
+module.exports.logoutUser=async (req,res,next)=>{
+    res.clearCookie('token');
+
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    await blaclistToken.create({token});
+
+    res.status(401).json({message :'logout'})
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const {body}=require('express-validator')
 const userController= require('../controllers/userController')
-
+const {authUser}=require('../middleware/authMiddleware');
 router.post('/register',[
     body('email').isEmail().withMessage('invalid email'),
     body('name.firstname').isLength({min:3}).withMessage('first name must be min 3 characters long'),
@@ -14,4 +14,7 @@ router.post('/login',[
     body('password').isLength({min:6}).withMessage('password must be 6 characters long')
 ],userController.loginUser);
 
+router.get('/profile',authUser,userController.getUserProfile);
+
+router.get('/logout',authUser,userController.logoutUser);
 module.exports=router;
