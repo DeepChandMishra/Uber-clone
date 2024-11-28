0bd6ae5ffc6bc80ef35c18e4f98d380d35daf277
DeepChandMishra <deepchandmishra90761@gmail.com>
2024-11-28 14:50:49 +0530

    user login done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const userModel=require('../models/user');
 const userService=require('../services/userService');
 const {validationResult}=require('express-validator')
+
 module.exports.registerUser= async (req,res,next)=>{
     const error=validationResult(req);
     if(!error.isEmpty()){
@@ -14,4 +15,27 @@ module.exports.registerUser= async (req,res,next)=>{
     });
     const token =user.generateAuthToken();
     res.status(201).json({token,user});
+}
+
+module.exports.loginUser= async(req,res,next)=>{
+    const error=validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(400).json({message:error});
+    }
+    const {email,password}=req.body;
+    const user= await userModel.findOne({email}).select('+password');
+
+    if(!user){
+        return res.status(401).json({message : 'invalid email or password'});
+    }
+
+    const isMatch=await user.comparePassword(password);
+
+    if(!isMatch){
+        return res.status(401).json({message : 'invalid email or password'});
+    }
+
+    const token=user.generateAuthToken();
+    
+    res.status(200).json({token,user});
 }
